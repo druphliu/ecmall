4a5c33e36970ee0ea4fa7dbf61a53c32c6149b57
hui.liu <980500610@qq.com>
2014-09-14 15:24:27 +0800

    wap theme

--- meishi/app/wap.app.php
+++ meishi/app/wap.app.php
@@ -7,13 +7,20 @@ class WapAPP extends MallbaseApp
         $this->display('wap/index.html');
     }
     function goodsList(){
+        $this->assign('title', '切换区域');
         $this->display('wap/goods.list.html');
     }
     function orderList(){
+        $this->assign('title', '切换区域');
         $this->display('wap/order.list.html');
     }
     function ucenter(){
+        $this->assign('title', '切换区域');
         $this->display('wap/ucenter.html');
+    }
+    function area(){
+        $this->assign('title', '切换区域');
+        $this->display('wap/area.html');
     }
 }
 
